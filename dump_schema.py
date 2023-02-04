9be424e412e9e86f653a75b523ea492b0db4721a
Joe Hansche <jhansche@themeetgroup.com>
2023-02-04 08:49:39 -0500

    Update schema

--- dump_schema.py
+++ dump_schema.py
@@ -3,6 +3,6 @@ import pprint
 
 from birdbuddy.client import BirdBuddy
 
-bb = BirdBuddy(email=None, password=None)
+bb = BirdBuddy()
 # Redirect output to schema.txt file
 pprint.pprint({"data": asyncio.run(bb.dump_schema())})
